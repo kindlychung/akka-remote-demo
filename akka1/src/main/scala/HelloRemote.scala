78dcd7c1c739d20d14d05c47285c28019015332a
Kaiyin Zhong <kindlychung@gmail.com>
2016-05-30 15:46:42 +0200

    first commit

---
+++ akka1/src/main/scala/HelloRemote.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by IDEA on 5/29/16.
+  */
+import akka.actor._
+
+object HelloRemote extends App  {
+  val system = ActorSystem("HelloRemoteSystem")
+  val remoteActor = system.actorOf(Props[RemoteActor], name = "RemoteActor")
+  remoteActor ! "The RemoteActor is alive"
+}
+
+class RemoteActor extends Actor {
+  def receive = {
+    case msg: String =>
+      println(s"RemoteActor received message '$msg'")
+      sender ! "Hello from the RemoteActor"
+  }
+}
